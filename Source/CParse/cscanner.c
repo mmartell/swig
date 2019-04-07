0720e782d98cde516ea31a61b244323928ebbaf0
Mike Martell <mmartell@pivotal.io>
2019-04-07 06:57:54 -0700

    Fix csanner.c for undefined DOXYGENSTRINGs.

--- Source/CParse/cscanner.c
+++ Source/CParse/cscanner.c
@@ -477,11 +477,11 @@ static int yylook(void) {
 
 	switch (existing_comment) {
 	  case DOX_COMMENT_PRE:
-	    return DOXYGENSTRING;
+	    return "DOXYGENSTRING";
 	  case DOX_COMMENT_NONE:
 	    break;
 	  case DOX_COMMENT_POST:
-	    return DOXYGENPOSTSTRING;
+	    return "DOXYGENPOSTSTRING";
 	}
       }
       break;
